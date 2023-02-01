aecc281a5ec3a562ab0d66e84ce896e1caa07529
tomifuchi <tomifuchi@yahoo.com>
2023-02-01 15:28:26 +0700

    First commit

---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+/*
+    Brain storming on how we could go about creating todos
+
+    *At a minimum they should have a title, description, dueDate and priority. 
+    You might also want to include notes or even a checklist.
+
+    Features for todos item:
+        Title
+        Description
+        Due date
+        Priority
+
+    * Our todo list should have projects or separate lists of todos. 
+    When a user first opens the app, there should be some sort of ‘default’ project to which all of their todos are put. 
+    Users should be able to create new projects and choose which project their todos go int
+
+    * Thinking with module
+
+    * UI is up to me
+
+
+    * What can it do ?
+        1 view all projects
+        2 view all todos in each project (probably just the title and duedate… perhaps changing color for different priorities)
+        3 expand a single todo to see/edit its details
+        4 delete a todo
+
+    Use local storage to persists data when reload, and can reload save file aswel. Try diving in see
+    what happens.
+
+
+    Very simple interface and foundation work can be work on right now
+    my todo's revolves around tagging things to categorize things. And ofcourse that's a feature to implement
+    later. NOw should be laying foundation that we can do this
+
+    Create a note
+    Add a note
+    Diplay that note
+    Delete that note
+    
+*/
+
+import * as pubsub from './modules/pubsub.js';
---
+++ src/js/modules/pubsub.js
@@ -0,0 +1,27 @@
+const _Subscriber = function(context, type, message, Fn) {
+    return Object.assign(
+        Object.create({type: "SubscriberInterface"}),
+        {context, type, message, callBackFunction: Fn}
+    )
+}
+
+const subscribers =  [];
+
+function subscribe(context, type, message, callBackFunction) {
+    subscribers.push(_Subscriber(context, type, message, callBackFunction));
+}
+
+function unsubscribe(context, type, message) {
+    subscribers.filter(item => item.context == context && item.type == type && item.message == message).forEach(item => subscribers.splice(subscribers.indexOf(item),1));
+}
+
+ //Can publish arguments for callbacks to use aswel
+function publish(type, message, args) {
+    subscribers.filter(sub => sub.type == type && sub.message == message).forEach(item => item.callBackFunction(args));
+}
+
+function pubsubTest() {
+    console.log('Hello from pubsub');
+}
+
+export {subscribe, unsubscribe, publish, pubsubTest};
